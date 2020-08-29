20752927ad68f7af29ead6a42ea3c27d2ce5ae28
Emily Wirtz <ewirtz3@gmail.com>
2020-08-29 14:00:10 -0500

    removing file path from css on Contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button, Form, Container, TextArea, Segment } from "semantic-ui-react";
-import "../assets/css/Contact.css";
+import "./Contact.css";
 import axios from "axios";
 
 export default function Contact() {
